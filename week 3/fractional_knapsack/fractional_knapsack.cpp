32b93ae2415b9583d55b96d7e5022f4e5c9aae71
Angela Wang <angelawang@raventech.com>
2017-01-16 12:32:54 +0800

    fractional_knapsack: comparing floating number

--- week 3/fractional_knapsack/fractional_knapsack.cpp
+++ week 3/fractional_knapsack/fractional_knapsack.cpp
@@ -1,17 +1,75 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+#include <string>
 
+using std::cout;
 using std::vector;
+
+vector<int> get_indices_by_value(vector<int> weights, vector<int> values) {
+  size_t size = weights.size();
+  vector<int> indices(size);
+
+  // insertion sort values by max to min
+  int tmp, high_proxy, high_index;
+  double max = 0.0;
+
+  for (int i = 0; i < size; i++) {
+    indices[i] = i;
+  }
+
+  for (int i = 0; i < size - 1; i++) {
+    high_proxy = indices[i];
+    high_index = i;
+
+    for (int j = i + 1; j < size; j++) {
+      if (values[indices[j]] * 1.0 / weights[indices[j]] >
+        values[high_proxy] * 1.0 / weights[high_proxy]) {
+        high_proxy = indices[j];
+        high_index = j;
+      }
+    }
+
+    tmp = indices[high_index];
+    indices[high_index] = indices[i];
+    indices[i] = tmp;
+  }
+
+  return indices;
+}
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  vector<int> indices = get_indices_by_value(weights, values);
 
-  // write your code here
+  for (int i = 0; i < indices.size(); i++) {
+    int pos = indices[i];
+    if (capacity >= weights[pos]) {
+      capacity -= weights[pos];
+      value += values[pos];
+    } else if (capacity < weights[pos]) {
+      value += values[pos] * 1.0 / weights[pos] * capacity;
+      capacity = 0;
+      break;
+    }
+  }
 
   return value;
 }
 
+void test_solution() {
+  assert(get_optimal_value(50, {20, 50, 30}, {60, 100, 120}) == 180);
+  assert(get_optimal_value(50, {30}, {500}) == 500);
+  char s1[10];
+  char s2[10];
+  sprintf(s1, "%.4f", get_optimal_value(10, {30}, {500}));
+  sprintf(s2, "%.4f", 500.0 / 3);
+  assert(strcmp(s1, s2) == 0);
+}
+
 int main() {
+  test_solution();
+
   int n;
   int capacity;
   std::cin >> n >> capacity;
--- week 3/fractional_knapsack/fractional_knapsack.py
+++ week 3/fractional_knapsack/fractional_knapsack.py
@@ -3,15 +3,39 @@ import sys
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
+
+    def get_max_value_index():
+        index = None
+        value = 0
+        for (i, v) in enumerate(values):
+            if weights[i] != 0 and v / weights[i] > value:
+                index = i
+                value = v / weights[i]
+        return index
+
+    # there are three possible cases for the knapsack:
+    # 1. the knapsack has same capacity as the loot
+    # 2. the knapsack has smaller capacity than the loot
+    # 3. the knapsack has greater capacity than the loot
+
+    item_index = get_max_value_index()
+    while capacity != 0 and item_index is not None: # handles case 3
+        if capacity >= weights[item_index]: # handles case 1 and 3
+            capacity -= weights[item_index]
+            value += values[item_index]
+            weights[item_index] = 0
+        else: # handles case 2
+            value += (values[item_index] * 1.0 / weights[item_index]) * capacity
+            capacity = 0
+            break
+        item_index = get_max_value_index()
 
     return value
 
+def test_solution():
+    assert get_optimal_value(50, [20, 50, 30], [60, 100, 120]) == 180
+    assert get_optimal_value(50, [30], [500]) == 500
+    assert '%.4f' % round(get_optimal_value(10, [30], [500]), 4) == '%.4f' % round(500.0 / 3, 4)
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    test_solution()
