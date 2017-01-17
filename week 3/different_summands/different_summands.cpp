daafd42db4c8528d80046af2314759a90f3ed43c
Angela Wang <angelawang@raventech.com>
2017-01-17 11:18:05 +0800

    different_summands

--- week 3/different_summands/different_summands.cpp
+++ week 3/different_summands/different_summands.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
 #include <vector>
-
-using std::vector;
+#include <cassert>
+using namespace std;
 
 vector<int> optimal_summands(int n) {
   vector<int> summands;
-  //write your code here
+
+  if (n <= 2) {
+    summands.push_back(n);
+  } else {
+    int l = 1;
+
+    while (n > 2 * l) {
+      summands.push_back(l);
+      n -= l++;
+    }
+
+    if (n > 0) {
+      summands.push_back(n);
+    }
+  }
+
   return summands;
 }
 
+void test_solution() {
+  vector<int> result;
+
+  result = optimal_summands(2);
+  assert(result.size() == 1);
+  assert(result[0] == 2);
+  result = optimal_summands(5);
+  assert(result.size() == 2);
+  assert(result[0] == 1);
+  assert(result[1] == 4);
+  result = optimal_summands(8);
+  assert(result.size() == 3);
+  assert(result[0] == 1);
+  assert(result[1] == 2);
+  assert(result[2] == 5);
+}
+
 int main() {
+  test_solution();
+
   int n;
   std::cin >> n;
   vector<int> summands = optimal_summands(n);
--- week 3/different_summands/different_summands.py
+++ week 3/different_summands/different_summands.py
@@ -2,11 +2,34 @@
 import sys
 
 def optimal_summands(n):
+    if n <= 2:
+        return [n]
+
+    l = 1
     summands = []
-    #write your code here
+
+    while n > 2 * l:
+        summands.append(l)
+        n -= l
+        l += 1
+
+    if n > 0:
+        summands.append(n)
+
     return summands
 
+def test_solution():
+    are_lists_identical(optimal_summands(2), [2])
+    are_lists_identical(optimal_summands(5), [1, 4])
+    are_lists_identical(optimal_summands(6), [1, 2, 3])
+    are_lists_identical(optimal_summands(8), [1, 2, 5])
+
+def are_lists_identical(l1, l2):
+    assert len(set(l1).union(l2)) == len(l1)
+
 if __name__ == '__main__':
+    test_solution()
+
     input = sys.stdin.read()
     n = int(input)
     summands = optimal_summands(n)
