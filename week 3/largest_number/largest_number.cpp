4d08c995fcc95678f6a063dcd1a10fd332dbfd19
Angela Wang <angelawang@raventech.com>
2017-01-17 14:35:48 +0800

    largest_number

--- week 3/largest_number/largest_number.cpp
+++ week 3/largest_number/largest_number.cpp
@@ -3,22 +3,49 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
 
 using std::vector;
 using std::string;
+using std::stoi;
 
 string largest_number(vector<string> a) {
   //write your code here
+  int k;
+  string temp;
   std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
+  size_t size = a.size();
+
+  for (int i = 0; i < size - 1; i++) {
+    k = i;
+    for (int j = i + 1; j < size; j++) {
+      if (stoi(a[j]) > stoi(a[k])) {
+        k = j;
+      }
+    }
+
+    temp = a[k];
+    a[k] = a[i];
+    a[i] = temp;
+  }
+
+  for (int i = 0; i < size; i++) {
     ret << a[i];
   }
+
   string result;
   ret >> result;
   return result;
 }
 
+void test_solution() {
+  assert(largest_number({"3", "4", "2"}) == "432");
+  assert(largest_number({"38", "4", "79"}) == "79384");
+}
+
 int main() {
+  test_solution();
+
   int n;
   std::cin >> n;
   vector<string> a(n);
--- week 3/largest_number/largest_number.py
+++ week 3/largest_number/largest_number.py
@@ -3,15 +3,24 @@
 import sys
 
 def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+    for i in range(len(a) - 1):
+        swap = i
+        for j in range(i + 1, len(a)):
+            if a[swap] < a[j]:
+                swap = j
+
+        a[swap], a[i] = a[i], a[swap]
+
+    return ''.join(map(str, a))
+
+def test_solution():
+    assert largest_number([3, 4, 2]) == '432'
+    assert largest_number([38, 4, 79]) == '79384'
 
 if __name__ == '__main__':
+    test_solution()
+
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
